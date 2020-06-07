7dfa8f9d7f564d7e223bbeab875910bac833049a
mritunjay kumar mani <mkumarmani97@gmail.com>
2020-06-08 00:52:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,20 +7,6 @@ window.addEventListener('keydown',function(e){
     key.classList.add("playing");
 });
 
-//In touchscreen
-window.addEventListener('touchstart', function(e){
-var touchlist = e.touches
-for (var i=0; i<touchlist.length; i++){
-	//loop through all touch points
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return // stop the function from running all together
-    audio.currentTime = 0;
-    audio.play();
-    window.
-    key.classList.add("playing")};
-});
-
 function removeTransition(e){
 	if (e.propertyName !== 'transform')
 		return
